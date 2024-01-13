10e9c3d91aaf56697186f0eb51df0cabfad39c4f
mostapha <mostaphayessf14@gmail.com>
2024-01-13 15:53:50 +0100

    home header

--- app/src/main/java/com/example/applicationmeteo/MainActivity.kt
+++ app/src/main/java/com/example/applicationmeteo/MainActivity.kt
@@ -1,4 +1,21 @@
 package com.example.applicationmeteo
 
-class MainActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.applicationmeteo.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    //injecter le fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.page_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+
 }
---
+++ app/src/main/java/com/example/applicationmeteo/fragments/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.applicationmeteo.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.applicationmeteo.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater?.inflate(R.layout.fragment_home_header , container, false)
+    }
+
+}

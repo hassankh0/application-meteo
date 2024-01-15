b8459c6e43d929ea9f1798455f8b5982a51878b5
hassankh0 <hassankhalife2001@icloud.com>
2024-01-15 09:28:16 +0100

    resolution problem launch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.applicationmeteo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.applicationmeteo"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 34
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

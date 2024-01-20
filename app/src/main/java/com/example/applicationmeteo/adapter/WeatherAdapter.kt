ea24c437ce58c07e810de99b2ff64c6fe9f44378
hassankh0 <hassankhalife2001@icloud.com>
2024-01-20 21:18:33 +0100

    implement the list

---
+++ app/src/main/java/com/example/applicationmeteo/adapter/WeatherAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.applicationmeteo.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applicationmeteo.R
+
+class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val weatherItem = view.findViewById<ImageView>(R.id.list_weather)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_meteo_list, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = 5
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //TODO
+    }
+}

7707cead422c7c5da2af7c5e9af712f4244773af
mostapha <mostaphayessf14@gmail.com>
2024-01-21 16:59:39 +0100

    WeekMeteoList

--- app/src/main/java/com/example/applicationmeteo/MainActivity.kt
+++ app/src/main/java/com/example/applicationmeteo/MainActivity.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.applicationmeteo.fragments.HomeFragment
+import com.example.applicationmeteo.fragments.WeekFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    //injecter le fragment
+
+        // Inject the HomeFragment
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.page_container, HomeFragment())
+        transaction.replace(R.id.page_container, WeekFragment())
         transaction.addToBackStack(null)
         transaction.commit()
-
     }
+
+
 
 }
